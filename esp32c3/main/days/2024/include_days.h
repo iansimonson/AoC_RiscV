8efcb8565a3ed4325b8d8b6a7742bac16ac9276a
Ian Simonson <ian@iansimonson.com>
2024-12-08 16:41:57 -0800

    fixup all days for esp32. day 7 still broken

--- esp32c3/main/days/2024/include_days.h
+++ esp32c3/main/days/2024/include_days.h
@@ -13,17 +13,31 @@ static void unimplemented(char *input, int len)
 
 extern void day1_part1(char *input, int len);
 extern void day1_part2(char *input, int len);
+extern void day2_part1(char *input, int len);
+extern void day2_part2(char *input, int len);
+extern void day3_part1(char *input, int len);
+extern void day3_part2(char *input, int len);
+extern void day4_part1(char *input, int len);
+extern void day4_part2(char *input, int len);
+extern void day5_part1(char *input, int len);
+extern void day5_part2(char *input, int len);
+extern void day6_part1(char *input, int len);
+extern void day6_part2(char *input, int len);
+extern void day7_part1(char *input, int len);
+extern void day7_part2(char *input, int len);
+extern void day8_part1(char *input, int len);
+extern void day8_part2(char *input, int len);
 
 
 solve_fn solutions_p1[25] = {
     day1_part1,
-    unimplemented,
-    unimplemented,
-    unimplemented,
-    unimplemented,
-    unimplemented,
-    unimplemented,
-    unimplemented,
+    day2_part1,
+    day3_part1,
+    day4_part1,
+    day5_part1,
+    day6_part1,
+    day7_part1,
+    day8_part1,
     unimplemented,
     unimplemented,
     unimplemented,
@@ -45,13 +59,13 @@ solve_fn solutions_p1[25] = {
 
 solve_fn solutions_p2[25] = {
     day1_part2,
-    unimplemented,
-    unimplemented,
-    unimplemented,
-    unimplemented,
-    unimplemented,
-    unimplemented,
-    unimplemented,
+    day2_part2,
+    day3_part2,
+    day4_part2,
+    day5_part2,
+    day6_part2,
+    day7_part2,
+    day8_part2,
     unimplemented,
     unimplemented,
     unimplemented,
